2939432f284f06d1864b0f4e03f7d87f29a7936c
Dev Khandelwal <122960451+khandelwaldev@users.noreply.github.com>
2023-11-17 20:59:16 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,13 @@ export default function Document() {
   return (
     <Html className="scroll-smooth">
       <Head>
-        <link
+        {/* <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css"
+        ></link> */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css"
         ></link>
       </Head>
       <body className="max-w-[672px] w-full mx-auto px-5 bg-[#011627] text-[#f6f7f8]">
