72b7eec895ae086fc3fc57000583090d2bafa47d
Dev Khandelwal <122960451+khandelwaldev@users.noreply.github.com>
2023-12-08 22:47:59 +0530

    Update NowPlaying.js

--- components/NowPlaying.js
+++ components/NowPlaying.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 import AnimatedLink from "./design/AnimatedLink";
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
 const NowPlaying = () => {
-  const { data } = useSWR("/api/spotify", fetcher);
+  const [forceRender, setForceRender] = useState(false);
+  const { data, mutate } = useSWR("/api/spotify", fetcher);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Force a re-render every 10 seconds
+      setForceRender((prev) => !prev);
+      // Manually re-fetch data to check for updates
+      mutate();
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!data) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
       <p className="flex gap-3 w-fit text-lg items-center">
-        {data?.isPlaying ? (
+        {data.isPlaying ? (
           <AnimatedLink href={data.songUrl} target="_blank" rel="noopener noreferrer">
             <div className="flex w-fit items-center gap-3">
               <img src={data.albumImageUrl} width={30} height={30} className="rounded-full" alt={data.title} />
@@ -18,10 +34,10 @@ const NowPlaying = () => {
             </div>
           </AnimatedLink>
         ) : (
-          <>
+          <div className="flex w-fit items-center gap-3">
             <img src="/spotify.png" width={30} height={30} className="rounded-full" alt="Not Listening - Spotify" />
             <span className="!text-white">Not Listening - Spotify</span>
-          </>
+          </div>
         )}
       </p>
     </div>
