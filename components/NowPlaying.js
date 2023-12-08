134c06eb5bf2d713e5e2d991e689f54cbb998755
Dev Khandelwal <122960451+khandelwaldev@users.noreply.github.com>
2023-12-08 22:36:57 +0530

    Update NowPlaying.js

--- components/NowPlaying.js
+++ components/NowPlaying.js
@@ -1,6 +1,5 @@
 import React from "react";
 import useSWR from "swr";
-import { SpotifyLogo } from "./Icons";
 import AnimatedLink from "./design/AnimatedLink";
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
@@ -10,14 +9,19 @@ const NowPlaying = () => {
 
   return (
     <div>
-       <p className="flex gap-3 w-fit text-lg items-center">
-        <SpotifyLogo className="bg-[#4CAF50] rounded-full text-black w-6 h-6" />
+      <p className="flex gap-3 w-fit text-lg items-center">
         {data?.isPlaying ? (
           <AnimatedLink href={data.songUrl} target="_blank" rel="noopener noreferrer">
-            <span  className="!text-white">{data.title}</span>
+            <div className="flex w-fit items-center gap-3">
+              <img src={data.albumImageUrl} width={30} height={30} className="rounded-full" alt={data.title} />
+              <span className="!text-white">{data.title}</span>
+            </div>
           </AnimatedLink>
         ) : (
-          "Not Listening - Spotify"
+          <>
+            <img src="/spotify.png" width={30} height={30} className="rounded-full" alt="Not Listening - Spotify" />
+            <span className="!text-white">Not Listening - Spotify</span>
+          </>
         )}
       </p>
     </div>
